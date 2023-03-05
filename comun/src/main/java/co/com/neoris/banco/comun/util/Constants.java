cf195075a10cd9669baff8cfe748e52e67f29553
Juan Diaz <juangdm1234@gmail.com>
2023-03-05 04:33:48 -0500

    Adicion de manejo de excepcion en servicio de consulta de cliente

--- comun/src/main/java/co/com/neoris/banco/comun/util/Constants.java
+++ comun/src/main/java/co/com/neoris/banco/comun/util/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 	private Constants() {}
 	
 	public static String NO_DATA_EXISTS = "No existe data.";
+	public static String ERROR_GET_CLIENTE = "Error al consultar el cliente.";
 }
--- gestor-cliente/src/main/java/co/com/neoris/banco/gestorcliente/service/impl/ClienteService.java
+++ gestor-cliente/src/main/java/co/com/neoris/banco/gestorcliente/service/impl/ClienteService.java
@@ -9,13 +9,16 @@ import org.springframework.stereotype.Service;
 import co.com.neoris.banco.comun.dto.ClienteDto;
 import co.com.neoris.banco.comun.dto.ResponseDto;
 import co.com.neoris.banco.comun.entity.ClienteEntity;
+import co.com.neoris.banco.comun.exception.CustomException;
 import co.com.neoris.banco.comun.mapper.ClienteMapper;
 import co.com.neoris.banco.comun.repository.ClienteRepository;
 import co.com.neoris.banco.comun.util.Constants;
 import co.com.neoris.banco.gestorcliente.service.IClienteService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @AllArgsConstructor
+@Slf4j
 @Service
 public class ClienteService implements IClienteService{
 
@@ -32,15 +35,20 @@ public class ClienteService implements IClienteService{
 	 */
 	@Override
 	public ResponseEntity<ResponseDto> getCliente(Integer identificacion) {
-		Optional<ClienteEntity> cliente = clienteRepository.findByPersonaEntity_identificacion(identificacion);
-		if(cliente.isPresent()) {
-			ClienteDto clienteDto=ClienteMapper.INSTANCE.entityToDto(cliente.get());
-			ResponseDto responseDto = ResponseDto.builder().menssage(HttpStatus.OK.name())
-					                 .codeResponse(HttpStatus.OK.value()).objectResponse(clienteDto).build();
-			return new ResponseEntity<>(responseDto,HttpStatus.OK);
+		try {
+			Optional<ClienteEntity> cliente = clienteRepository.findByPersonaEntity_identificacion(identificacion);
+			if(cliente.isPresent()) {
+				ClienteDto clienteDto=ClienteMapper.INSTANCE.entityToDto(cliente.get());
+				ResponseDto responseDto = ResponseDto.builder().menssage(HttpStatus.OK.name())
+						                 .codeResponse(HttpStatus.OK.value()).objectResponse(clienteDto).build();
+				return new ResponseEntity<>(responseDto,HttpStatus.OK);
+			}
+			return new ResponseEntity<>(ResponseDto.builder().menssage(Constants.NO_DATA_EXISTS)
+	                	.codeResponse(HttpStatus.NOT_FOUND.value()).objectResponse(null).build(),HttpStatus.NOT_FOUND);
+		}catch(Exception ex) {
+			log.error(Constants.ERROR_GET_CLIENTE,ex);
+			throw new CustomException(Constants.ERROR_GET_CLIENTE+ex.getMessage());
 		}
-		return new ResponseEntity<>(ResponseDto.builder().menssage(Constants.NO_DATA_EXISTS)
-                	.codeResponse(HttpStatus.NOT_FOUND.value()).objectResponse(null).build(),HttpStatus.NOT_FOUND);
 	}
 
 	@Override
